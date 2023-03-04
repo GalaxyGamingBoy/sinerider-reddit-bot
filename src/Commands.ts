29640110bee8dbad28eb68e7203083d0ef73c32e
GalaxyGamingBoy <xrteach@gmail.com>
2023-03-04 11:48:16 +0200

    Integrated scoring server

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,17 +1,33 @@
 import {Command} from './types/Command';
 import {RegExpLib} from './RegExpLib';
+import axios from 'axios';
 
 export const Commands: Array<Command> = [
   {
     handler: RegExp('help'),
     command: comment => {
+      console.log('REPLYING TO COMMENT: ' + comment.permalink);
       comment.reply('Sinerider HELP');
     },
   },
   {
     handler: RegExpLib.URL.regexp,
     command: comment => {
-      comment.reply('URL DETECTED!');
+      const url = comment.body.split(' ')[1];
+      console.log('REPLYING TO COMMENT: ' + comment.permalink);
+      axios
+        .post(process.env.S_SCORINGSERVER + '/score', {level: url})
+        .then(response => {
+          if (response.data.success === true) {
+            comment.reply(
+              `Success! You did it! Here is your stats: Level: ${response.data.level}, T: ${response.data.T}, CharCount: ${response.data.charCount}. Play it [here](${url})`
+            );
+          } else {
+            comment.reply(
+              `Fail! :( You can try again! You go it. Review it [here](${url})`
+            );
+          }
+        });
     },
   },
 ];
