c690bbd2be6395078bd984f769605a60e4cafe0e
GalaxyGamingBoy <xrteach@gmail.com>
2023-03-13 17:50:11 +0200

    feat: Latest info

--- src/Commands.ts
+++ src/Commands.ts
@@ -16,12 +16,28 @@ export const Commands: Array<Command> = [
       const url = comment.body.split(' ')[1];
       console.log('REPLYING TO COMMENT: ' + comment.permalink);
       axios
-        .post(process.env.S_SCORINGSERVER + '/score', {level: url})
+        .post(
+          process.env.S_SCORINGSERVER + '/score',
+          {
+            level: url,
+          },
+          {
+            headers: {
+              'User-Agent': 'redditbot',
+            },
+          }
+        )
         .then(response => {
           if (response.data.success === true) {
-            comment.reply(
-              `Success! You did it! Here is your stats: Level: ${response.data.level}, T: ${response.data.T}, CharCount: ${response.data.charCount}. Play it [here](${url})`
-            );
+            if (response.data.gameplay === '') {
+              comment.reply(
+                `Success! You did it! Here is your stats: Level: ${response.data.level}, T: ${response.data.T}, CharCount: ${response.data.charCount}. Play it [here](${url})`
+              );
+            } else {
+              comment.reply(
+                `Success! You did it! Here is your stats: Level: ${response.data.level}, T: ${response.data.T}, CharCount: ${response.data.charCount}. Watch it [here](${response.data.gameplay}) or play it [here](${url})`
+              );
+            }
           } else {
             comment.reply(
               `Fail! :( You can try again! You go it. Review it [here](${url})`
